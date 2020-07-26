dfcc684f1786c06805fb84fa7cf057baf76ee7f9
vplacido <valentinplacido25@gmail.com>
2020-07-25 23:26:10 -0400

    setup basic signup form in signup.js'

---
+++ src/components/Authentication/SignUpFormContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class SignUpFormContainer extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default SignUpFormContainer
