cb57e77572bf63f101929632abaa6c301d8c0e07
vplacido <valentinplacido25@gmail.com>
2020-07-25 23:04:06 -0400

    imported the proper files to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import Home from './components/Home'
+import LoginFormContainer from './components/Authentication/LoginFormContainer'
+import SignUpFormContainer from './components/Authentication/SignUpFormContainer'
+import ListingContainer from './components/Listings/ListingContainer'
+import ListingPage from './components/Listings/item/ListingPage'
+import NewListingForm from './components/Listings/NewListingForm'
 import NavBar from './components/NavBar/NavBar'
+import TripContainer from './components/TripsContainer'
 
 function App() {
   return (
